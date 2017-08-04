7cd9f28845bc724d24c0b91a149e6102c3f5d36a
shiguang328 <13052353610@163.com>
2017-08-04 18:23:37 +0800

    finish the first microblog write by flask

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -2,8 +2,12 @@ import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
+from flask_script import Manager
 
 app = Flask(__name__) # create the application instance
+
+manager = Manager(app)
+
 app.config.from_object(__name__)  # load config file from this file, microblog.py
 # Load default config and override config from an environment variable
 app.config.update(dict(
@@ -45,5 +49,45 @@ def initdb_command():
     init_db()
     print('Initializes the database.')
 
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title,text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title,text) values (?, ?)',
+                [request.form['title'],request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    print('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return  redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
 if __name__ == '__main__':
+    #manager.run()
     app.run()
