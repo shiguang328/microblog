07c2e88541b355fbd66b6d0078c2d9119b4dc37b
shiguang328 <13052353610@163.com>
2017-08-04 13:57:30 +0800

    add files

---
+++ microblog/__init__.py
@@ -0,0 +1,1 @@
+from .microblog import app
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name = 'microblog',
+    packages=['microblog'],
+    include_package_data=True,
+    install_requires=['microblog',],
+)
